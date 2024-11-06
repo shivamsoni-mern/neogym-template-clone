e49fb3eddee159c9fc543adb30c5fa537306e3fb
shivamsoni-mern <shivamsoni082002@gmail.com>
2024-11-06 20:35:27 +0530

    first commit

---
+++ trainers.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+<head>
+<title>web design</title>
+<link type="text/css" rel="stylesheet" href="fontawesome-free-6.5.1-web/css/all.min.css">
+<link type="text/css" rel="stylesheet" href="css/neogym.css">
+</head>
+<body>
+<!--banner-start -->
+<div class="banner-section inner-page-header">
+	<div class="row">
+   <div class="col-1"> 
+	<h2 class="neogym-heading"> NEOGYM </h2>
+   </div>
+   <div class="col-2">
+   <ul class="banner-list">
+			<li> <a href=""> HOME </a></li>
+			<li> <a href=""> WHY US </a></li>
+			<li> <a href=""> TRAINERS</a></li>
+			<li> <a href=""> CONTACT US </a></li>
+			<li> <a href=""><i class="fa-solid fa-magnifying-glass"></i></a></li>
+   </ul>
+   </div>
+   </div>
+   </div>
+
+
+<div class="last-banner">
+	<div class="container">
+			<div class="row">
+			<div class="last-banner-col">
+				<div class="last-banner-box">
+					<img src="images/location-white.png"  class="suppliments-img-1">
+					<h3 class="last-banner-heading"> LOCATION</h3>
+					</div>
+				</div>
+				<div class="last-banner-col">
+					<div class="last-banner-box">
+					<img src="images/telephone-white.png"  class="suppliments-img-1">
+					<h3 class="last-banner-heading"> +919691522638</h3>
+					</div>
+				</div>
+				<div class="last-banner-col">
+					<div class="last-banner-box">
+					<img src="images/envelope-white.png"  class="suppliments-img-1">
+					<h3 class="last-banner-heading"> DEMO@GMAIL.COM</h3>
+					</div>
+				</div>
+		</div>
+	</div>
+</div>
+<!--copyright-start -->
+<div class="copyright-banner">
+	<div class="container">
+		<h2 class="copyright-heading">	© 2020 All Rights Reserved. Design by Free Html Templates </h2>
+	</div>
+</div>
+</body>
+</html> 
